00e6fd186494accc2e873c2810d4b088c189cd12
AbodHana2 <abodhanna0@gmail.com>
2022-12-09 14:11:22 +0200

    Creating new Methods and Views

--- BET KANU/Controllers/MangerController.cs
+++ BET KANU/Controllers/MangerController.cs
@@ -1,15 +1,18 @@
 ﻿using BetKanu.Models;
 using BetKanu.Models.Interface;
 using Microsoft.AspNetCore.Mvc;
+using System.Web.Helpers;
 
 namespace BET_KANU.Controllers
 {
     public class MangerController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public MangerController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public MangerController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public ActionResult Index()
         {
@@ -21,13 +24,40 @@ namespace BET_KANU.Controllers
             return View(new Product());
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,IFormFile SmallImage, IFormFile CoverImage)
         {
             if (ModelState.IsValid)
             {
+                if(SmallImage != null)
+                {
+                    var Serverpath = @"/img/Product/";
+                    var filename = $"{product.Id}_{product.Title}";
+                    string newfile = SaveImage(SmallImage, Serverpath, filename, false);
+                    product.SmallImage = Serverpath + filename + SmallImage.FileName.Substring(SmallImage.FileName.LastIndexOf('.'),4);
+                }
+                if(CoverImage != null)
+                {
+                    var Serverpath = @"/img/Product/";
+                    var filename = $"{product.Id}_{product.Title}";
+                    string newfile = SaveImage(SmallImage, Serverpath, filename, false);
+                    product.CoverImage = Serverpath + filename + SmallImage.FileName.Substring(SmallImage.FileName.LastIndexOf('.'), 4);
+                }
                 _unitOfWork.manger.Add(product);
             }
             return View(product);
+        }
+        private string SaveImage(IFormFile file, string Serverpath, string name,bool isCompletePath)
+        {
+            WebImage img = new WebImage(file.ContentDisposition);
+            string webRoot = _webHostEnvironment.WebRootPath;
+            string? getFileName;
+            getFileName = file.FileName;
+            var Filename = Path.GetFileName(file.FileName);
+            string newFileName = name.ToString() + Filename.Substring(Filename.LastIndexOf("."), 4);
+            var path = Path.Combine((isCompletePath ? Serverpath : webRoot), newFileName);
+            img.Save(path);
+
+            return newFileName;
         }
         public ActionResult Edit(int id)
         {
@@ -52,6 +82,30 @@ namespace BET_KANU.Controllers
                 return RedirectToAction("Index");
             }
             return View(product);
+        }
+
+       
+        public ActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var prod = _unitOfWork.product.GetOne(id);
+            if (prod == null)
+            {
+                return NotFound();
+            }
+            return View(prod);
+        }
+
+        public ActionResult DeletePost(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.manger.Delete(id);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
