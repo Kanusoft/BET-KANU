7e6dd5c54d0165e965bb9fd5efd4a3a18e0209d9
marine <marine.krikorian.2@gmail.com>
2024-01-16 14:22:47 +0300

    remove

--- BET KANU/wwwroot/plugins/fileuploads/js/dropify.js
+++ BET KANU/wwwroot/plugins/fileuploads/js/dropify.js
@@ -66,7 +66,7 @@ function Dropify(element, options) {
             message:         '<div class="dropify-message"><span class="file-icon" /> <p>{{ default }}</p></div>',
             preview:         '<div class="dropify-preview"><span class="dropify-render"></span><div class="dropify-infos"><div class="dropify-infos-inner"><p class="dropify-infos-message">{{ replace }}</p></div></div></div>',
             filename:        '<p class="dropify-filename"><span class="dropify-filename-inner"></span></p>',
-            clearButton:     '<button type="button" class="dropify-clear">{{ remove }}</button>',
+/*            clearButton:     '<button type="button" class="dropify-clear">{{ remove }}</button>',*/
             errorLine:       '<p class="dropify-error">{{ error }}</p>',
             errorsContainer: '<div class="dropify-errors-container"><ul></ul></div>'
         }
